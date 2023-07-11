550ec1c18547f26fe608750e588844926059b58e
PlaceboXJ <139255970+PlaceboXJ@users.noreply.github.com>
2023-07-11 22:37:53 +0300

    jsCalc_jexon228.js

---
+++ jsCalc_jexon228.js
@@ -0,0 +1,36 @@
+
+<script>
+let input
+let first=parseInt(prompt())
+let second=parseInt(prompt())
+let operator=prompt()
+switch (operator){
+    case "+":{
+        input=first+second  
+        break       
+    } 
+    case "-":{
+        input=first-second  
+        break       
+    }
+
+
+}
+
+
+alert(input)
+
+
+
+
+
+
+
+
+
+</script>
+
+
+
+
+
